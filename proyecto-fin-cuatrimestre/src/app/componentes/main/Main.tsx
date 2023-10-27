c9063d84e44095bed3e82cdc8a9f9a8eb4f268bd
ArielGR2 <arielgromera@gmail.com>
2023-10-27 18:46:10 -0300

    Agrego titulo a Main

--- proyecto-fin-cuatrimestre/src/app/componentes/main/Main.tsx
+++ proyecto-fin-cuatrimestre/src/app/componentes/main/Main.tsx
@@ -6,7 +6,11 @@ export const Main = () => {
   return (
     <>    
       <div className='cuerpo' >Main
-      
+        <div>
+        <h1 className='tituloCuerpo'>Municipio de Olavarria</h1>
+        </div>
+        
+
       </div>      
     </>
   )
