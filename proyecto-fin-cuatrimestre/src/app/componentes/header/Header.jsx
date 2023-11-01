3f86a9cd42302d8f8c9010a86ba5d52da4dba7e7
Lindner_Mario <mario.l.lindner@gmail.com>
2023-11-01 17:57:39 -0300

    Actualizo Header

--- proyecto-fin-cuatrimestre/src/app/componentes/header/Header.jsx
+++ proyecto-fin-cuatrimestre/src/app/componentes/header/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import "./Header.css";
+import { ContRedesSociales } from "./RedesSociales/ContRedesSociales";
 
 export const Header = () => {
   return (
     <>
-    <div className="cabecera" >Header
-    
-    
-    
-    </div>
+      <div className="cabecera">
+        <div>
+          <h1>Municipio</h1>
+          <ContRedesSociales />
+        </div>
+        <div className="LogoMunicipio">
+          <img  src="" alt="Logo" />
+        </div>
+      </div>
     </>
   )
 }
---
+++ proyecto-fin-cuatrimestre/src/app/componentes/header/RedesSociales/ContRedesSociales.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { BotonRedesHeader } from "./Botones/BotonesHeader";
+import "./ContRedesSociales.css";
+import Facebook from "../../../../public/Facebook.png";
+
+export const ContRedesSociales = () => {
+  return (
+    <>
+      <div>
+        <nav className="ContRedesSociales">
+          <BotonRedesHeader
+            imageSrc = {Facebook.src}
+            linkTo="https://www.facebook.com/municipiodeolavarria"
+            altText="Facebook"
+          />
+          <BotonRedesHeader
+            imageSrc="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png"
+            linkTo="https://www.instagram.com/muniolav/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA=="
+            altText="Instagram"
+          />
+          <BotonRedesHeader
+            imageSrc="https://upload.wikimedia.org/wikipedia/commons/6/61/Logo_X.png"
+            linkTo="https://twitter.com/MuniOlav"
+            altText="X"
+          />
+          <BotonRedesHeader
+            imageSrc="https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png"
+            linkTo="https://www.youtube.com/@CiudadOlavarria"
+            altText="Youtube"
+          />
+        </nav>
+      </div>
+    </>
+  );
+};
