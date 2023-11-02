4482ad2dced39c57243b604217445bef840bc0d3
KevinFrias051 <kevinfrias051@gmail.com>
2023-11-02 17:38:37 -0300

    Intalacion de boostrap

--- proyecto-fin-cuatrimestre/src/app/page.tsx
+++ proyecto-fin-cuatrimestre/src/app/page.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from "./componentes/header/Header"
 import { Main } from "./componentes/main/Main"
 import { Footer } from "./componentes/footer/Footer"
