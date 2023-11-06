21f95f54a61deca0b99356fcbdd8833f6c78cc18
KevinFrias051 <kevinfrias051@gmail.com>
2023-11-06 14:12:37 -0300

    canvios varios en el footer

--- proyecto-fin-cuatrimestre/src/app/componentes/footer/Footer.jsx
+++ proyecto-fin-cuatrimestre/src/app/componentes/footer/Footer.jsx
@@ -5,24 +5,27 @@ export const Footer = () => {
   return (
     <>
     <div className='footer'>
-      <div className='pie-enlaces'>Enlaces de Interes
+      <div className='pie-enlaces'>
+        <p>Enlaces de Interes</p>
         <a href="http://www.olavarria.gov.ar/olavarria-interactiva/">Olavarria Interactiva</a>
         <a href="https://gobiernoabierto.olavarria.gov.ar/tramites-y-servicios/#licencias-de-conducir">Licencia de Conducir</a>
         <a href="http://https://gobiernoabierto.olavarria.gov.ar/tramites-y-servicios/">Guía de Trámites</a>
         <a href="http://https://hcd.olavarria.gov.ar/">Concejo Deliberante</a>
       </div>
-      <div className='pie-areas'>Areas de Gobierno
+      <div className='pie-areas'>
+        <p>Areas de Gobierno</p>
         {/* <a href="http://gobiernoabierto.olavarria.gov.ar/visualizaciones-4/organigrama/">Organigrama</a> */}
         <a href="https://www.olavarria.gov.ar/desarrollo-economico/empresas/">Desarrollo Económico</a>
         <a href="https://www.olavarria.gov.ar/agenda-municipal/">Cultura</a>
         <a href="https://www.olavarria.gov.ar/turismo/">Turismo</a>
         <a href="https://gobiernoabierto.olavarria.gov.ar/tramites-y-servicios/#salud/">Salud</a>
       </div>
-      <div className='pie-telefonos'>Telefonos Utiles
-        <p>Policia - 101</p>
-        <p>Bomberos - 100</p>
-        <p>SAME - 107</p>
-        <p>Emergencias - 911</p>
+      <div className='pie-telefonos'>
+        <p>Telefonos Utiles</p>
+        <a>Policia  - 101</a>
+        <a>Bomberos - 100</a>
+        <a>SAME     - 107</a>
+        <a>Emergencias - 911</a>
       </div>
       </div>
 
