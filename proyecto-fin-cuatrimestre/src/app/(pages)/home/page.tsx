9fa394077fe68773c46d68266beb73118143be23
Franco_Serjen <francoserjen100@gmail.com>
2023-11-15 17:36:14 -0300

    Navegar a home desde el logo principal

--- proyecto-fin-cuatrimestre/src/app/(pages)/home/page.tsx
+++ proyecto-fin-cuatrimestre/src/app/(pages)/home/page.tsx
@@ -3,7 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Main } from "../.././componentes/main/Main";
 import { useRouter } from 'next/navigation';
 
-
 export default function Home() {
     const router = useRouter();
   return (
--- proyecto-fin-cuatrimestre/src/app/componentes/header/Header.jsx
+++ proyecto-fin-cuatrimestre/src/app/componentes/header/Header.jsx
@@ -1,27 +1,45 @@
-"use client"
-import React from 'react';
+"use client";
+import React from "react";
 import "./Header.css";
 import { ContRedesSociales } from "./RedesSociales/ContRedesSociales";
-import Logo3 from "../../../../public/Logo3.png"
-import Logo2 from "../../../../public/Logo2.png"
-
+import Logo3 from "../../../../public/Logo3.png";
+import Logo2 from "../../../../public/Logo2.png";
+import { useRouter, usePathname } from "next/navigation";
 
 export const Header = () => {
+  //Constantes para navegar (router, pathname)
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const navegarAHome = () => {
+    if (pathname !== "/home") {
+      router.push("/home");
+    }
+  };
 
   return (
     <>
       <div className="cabecera">
-        <div className='DivNombreYRedes'>
-          <img className="LogoMunicipio2" src={Logo2.src} alt="LogoMunicipio2"/>
-          <ContRedesSociales className='Redes' />
+        <div className="DivNombreYRedes">
+          <img
+            className="LogoMunicipio2"
+            onClick={() => navegarAHome()}
+            src={Logo2.src}
+            alt="LogoMunicipio2"
+          />
+          <ContRedesSociales className="Redes" />
         </div>
         <div className="divTitulo">
           <h1>Bienvenidos a Olavarria</h1>
         </div>
-        <div className='DivLogo'>
-          <img className="LogoMunicipio1" src={Logo3.src} alt="LogoMunicipio1" />
+        <div className="DivLogo">
+          <img
+            className="LogoMunicipio1"
+            src={Logo3.src}
+            alt="LogoMunicipio1"
+          />
         </div>
       </div>
     </>
-  )
-}
+  );
+};
