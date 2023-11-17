534755d03ea99998261a3a02a8972bf74baf8944
Lindner_Mario <mario.l.lindner@gmail.com>
2023-11-17 18:09:32 -0300

    Cambio de Card Candidato

--- proyecto-fin-cuatrimestre/src/app/componentes/cardCandidato/cardCandidato.tsx
+++ proyecto-fin-cuatrimestre/src/app/componentes/cardCandidato/cardCandidato.tsx
@@ -1,24 +1,35 @@
+import { useState } from "react";
 import "./cardCandidato.css"
 
 export function CardEstilada(props: any) {
     const { srcImg, cardName, cardPuesto, cardLocalication, cardSkills, } = props;
+    
+    const [contrato,setContrato] = useState<String>("Contratar");
+    const contratar = () => {
+        if (contrato == "Contratar"){
+        setContrato("Despedir");
+        } else {
+            setContrato("Contratar");
+        }
+    }
     return (
         <>
             <div className="card-container">
                 <img className="round" src={srcImg} alt="user" />
                 <h3>{cardName}</h3>
-                <h6>{cardLocalication}</h6>
                 <p>{cardPuesto}</p>
+                <p>Edad</p>
+                <p>Movilidad</p>
+                <p>disponibilidad</p>
                 <div className="buttons">
-                    <button className="primary">
-                        Contratar
+                    <button className="primary" onClick={contratar}>
+                        {contrato}
                     </button>
                     <button className="primary ghost">
-                        Saber Mas
+                        <a href="https://www.linkedin.com/home" target="_blank">Linkedin</a>
                     </button>
                 </div>
                 <div className="skills">
-                    <h6>Skills</h6>
                     <ul>
                         <li>Front End Development</li>
                         <li>HTML</li>
