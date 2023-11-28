04948265e685080bdf208a093a6b1075b2009b97
KevinFrias051 <kevinfrias051@gmail.com>
2023-11-28 20:59:59 -0300

    HECHO

--- proyecto-fin-cuatrimestre/src/app/componentes/filtros/FiltroCandidatos.tsx
+++ proyecto-fin-cuatrimestre/src/app/componentes/filtros/FiltroCandidatos.tsx
@@ -1,50 +1,68 @@
-"use client"
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import Slider from '@mui/material/Slider';
 import InputLabel from '@mui/material/InputLabel';
-import './Filtro.css'
+import './Filtro.css';
 
 export function Filtro({ filtrarCandidatos, cargarCandidato }: any) {
-
-  const [disponibilidad, setDisponibilidad] = useState<boolean>();
-  const [movilidad, setMovilidad] = useState<boolean>();
+  const [disponibilidad, setDisponibilidad] = useState<boolean | undefined>(undefined);
+  const [movilidad, setMovilidad] = useState<boolean | undefined>(undefined);
   const [edad, setEdad] = useState([18, 65]);
 
   const handleDisponibilidadChange = (e) => {
-    e.target.value == "Full-Time" ? setDisponibilidad(true) : setDisponibilidad(false);
+    if (e.target.value === '') {
+      setDisponibilidad(undefined);
+    } else {
+      e.target.value === 'Full-Time' ? setDisponibilidad(true) : setDisponibilidad(false);
+    }
   };
 
   const handleMovilidadChange = (e) => {
-    e.target.value == "Si" ? setMovilidad(true) : setMovilidad(false);
+    if (e.target.value === '') {
+      setMovilidad(undefined);
+    } else {
+      e.target.value === 'Si' ? setMovilidad(true) : setMovilidad(false);
+    }
   };
 
-  const handleEdadChange = (e) => {
-    setEdad(e.target.value);
-
+  const handleEdadChange = (e, newValue) => {
+    setEdad(newValue);
   };
 
-
+  const reset = () => {
+    setDisponibilidad(undefined);
+    setMovilidad(undefined);
+    setEdad([18, 65]);
+    cargarCandidato();
+  };
 
   return (
     <div className='divFiltroCandidatos'>
       <InputLabel id="Disponibilidad">Disponibilidad</InputLabel>
-      <Select className='selectorFiltro' value={disponibilidad} defaultValue={"Seleccione"} onChange={handleDisponibilidadChange}>
-        <MenuItem className='selectorFiltro' value="Seleccione">Seleccione</MenuItem>
-        <MenuItem className='selectorFiltro' value="Full-Time">Full-Time</MenuItem>
-        <MenuItem className='selectorFiltro' value="Part-Time">Part-Time</MenuItem>
+      <Select
+        className='selectorFiltro'
+        value={disponibilidad === undefined ? 'Seleccione' : disponibilidad ? 'Full-Time' : 'Part-Time'}
+        onChange={handleDisponibilidadChange}
+      >
+        <MenuItem className='selectorFiltro' value='Seleccione'>Seleccione</MenuItem>
+        <MenuItem className='selectorFiltro' value='Full-Time'>Full-time</MenuItem>
+        <MenuItem className='selectorFiltro' value='Part-Time'>Part-Time</MenuItem>
       </Select>
       <InputLabel id="Movilidad">Movilidad</InputLabel>
-      <Select className='selectorFiltro' label="Movilidad" defaultValue={"Seleccione"} onChange={handleMovilidadChange}>
-        <MenuItem className='selectorFiltro' value="Seleccione">Seleccione</MenuItem>
-        <MenuItem className='selectorFiltro' value="Si">Si</MenuItem>
-        <MenuItem className='selectorFiltro' value="No">No</MenuItem>
+      <Select
+        className='selectorFiltro'
+        value={movilidad === undefined ? 'Seleccione' : movilidad ? 'Si' : 'No'}
+        onChange={handleMovilidadChange}
+      >
+        <MenuItem className='selectorFiltro' value='Seleccione'>Seleccione</MenuItem>
+        <MenuItem className='selectorFiltro' value='Si'>Si</MenuItem>
+        <MenuItem className='selectorFiltro' value='No'>No</MenuItem>
       </Select>
       <InputLabel id="RangoEdad">Edad (18-65)</InputLabel>
-      <Slider className='sliderEdad' value={edad} onChange={handleEdadChange} min={18} max={65} valueLabelDisplay="auto" />
+      <Slider className='sliderEdad' value={edad} onChange={handleEdadChange} min={18} max={65} valueLabelDisplay='auto' />
       <button className='btn' onClick={() => filtrarCandidatos({ name: null, edad, disponibilidad, movilidad })}>Buscar</button>
-      <button className="btn" onClick={cargarCandidato} >Reset</button>
+      <button className="btn" onClick={reset}>Reset</button>
     </div>
   );
 }
